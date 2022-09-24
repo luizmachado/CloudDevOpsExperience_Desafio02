a7da669da95fcb2aef088a8ee85847aa71459f7c
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-09-24 00:07:31 -0300

    Corrigir pasta de origem

--- create_env.sh
+++ create_env.sh
@@ -19,7 +19,7 @@ wget $URL -P /tmp/
 unzip /tmp/main.zip -d /tmp/
 
 #Copiar arquivos da aplicação para root do Apache
-cp -r /tmp/linux-site-dio/ /var/www/html/
+cp -r /tmp/linux-site-dio-main/ /var/www/html/
 
 #Habilitar e inicializar serviço do Apache
 systemctl enable apache2 && systemctl restart apache2
