0266eb3baa3599ef203f6d9ef08bf1827139c183
Luiz Augusto Machado <luizmachado.to@gmail.com>
2022-09-24 01:40:11 -0300

    Adicionado echos

--- create_env.sh
+++ create_env.sh
@@ -5,29 +5,37 @@ URL="https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.z
 
 # Configurar needrestart daemon para auto
 #e evitar interrupção do script após instalações
+echo "Configurando Needrestart"
 cp /etc/needrestart/needrestart.conf /etc/needrestart/needrestart.conf.bkp
 sed -i 's/#$nrconf{restart} = \x27i\x27/$nrconf{restart} = \x27a\x27/g' /etc/needrestart/needrestart.conf
 
 # Atualizar servidor
+echo "Atualizando servidor"
 apt update && apt upgrade -y
 
 # Instalar Apache
+echo "Instalando Apache e dependências"
 apt install apache2 -y
 
 # Instalar unzip
 apt install unzip -y
 
 # Download da aplicação
+echo "Obtendo aplicação"
 wget $URL -P /tmp/
 
 # Extrair arquivos
+echo "Extraindo arquivos e copiando para servidor web"
 unzip /tmp/main.zip -d /tmp/
 
 #Copiar arquivos da aplicação para root do Apache
 cp -r /tmp/linux-site-dio-main/* /var/www/html/
 
 #Habilitar e inicializar serviço do Apache
+echo "Habilitando servidor web"
 systemctl enable apache2 && systemctl restart apache2
+echo "Instalação concluída"
+
 
 
 
